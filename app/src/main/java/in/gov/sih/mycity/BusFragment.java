e844a37889dfdd2e736251e350988e5aef05c0cd
purusharth <purusharthugra@gmail.com>
2019-03-03 04:35:20 +0530

    Add BusFragment

--- app/src/main/java/in/gov/sih/mycity/BusFragment.java
+++ app/src/main/java/in/gov/sih/mycity/BusFragment.java
@@ -2,6 +2,7 @@ package in.gov.sih.mycity;
 
 import android.app.DatePickerDialog;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
@@ -9,10 +10,15 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ArrayAdapter;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import java.text.SimpleDateFormat;
@@ -27,6 +33,8 @@ public class BusFragment extends Fragment {
     private String city;
     private EditText edittext;
     private Calendar myCalendar;
+    private Spinner spinner;
+    private TextView show;
 
     public BusFragment() {
         // Required empty public constructor
@@ -41,7 +49,7 @@ public class BusFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View returnView = inflater.inflate(R.layout.fragment_bus, container,
+        final View returnView = inflater.inflate(R.layout.fragment_bus, container,
                 false);
         sourceView = (TextView) returnView.findViewById(R.id.source);
         context = getContext();
@@ -77,6 +85,25 @@ public class BusFragment extends Fragment {
             }
         });
 
+        String[] cities = new String[]{"Aligargh", "Agra", "Jaipur", "Kanpur", "Lucknow"};
+
+        spinner = (Spinner) returnView.findViewById(R.id.destination);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_spinner_item, cities);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        show = (TextView) returnView.findViewById(R.id.show);
+
+        show.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url = getUrl();
+                Intent intent = new Intent(getActivity(), WebViewActivity.class);
+                intent.putExtra("url", url);
+                startActivity(intent);
+            }
+        });
 
         return returnView;
     }
@@ -86,5 +113,67 @@ public class BusFragment extends Fragment {
         SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
 
         edittext.setText(sdf.format(myCalendar.getTime()));
+    }
+
+    private String getMonth(int i){
+        switch (i) {
+            case 1:
+                return "Jan";
+            case 2:
+                return "Feb";
+            case 3:
+                return "Mar";
+            case 4:
+                return "Apr";
+            case 5:
+                return "May";
+            case 6:
+                return "Jun";
+            case 7:
+                return "Jul";
+            case 8:
+                return "Aug";
+            case 9:
+                return "Sep";
+            case 10:
+                return "Oct";
+            case 11:
+                return "Nov";
+            default:
+                return "Dec";
+
+        }
+    }
+
+    private String getUrl(){
+        String url = "";
+        int choice = spinner.getSelectedItemPosition();
+        int targetCityId = 0;
+        switch (choice){
+            case 0:
+                targetCityId = 79613;
+                break;
+            case 1:
+                targetCityId = 1290;
+                break;
+            case 2:
+                targetCityId = 807;
+                break;
+            case 3:
+                targetCityId = 1377;
+                break;
+            default:
+                targetCityId = 1439;
+        }
+
+        String date = edittext.getEditableText().toString();
+        date = date.substring(0, 2) + "-" +  getMonth((Integer.parseInt(date.substring(3, 5))))
+                + "-" + "20" + date.substring(6);
+
+        url = "https://www.redbus.in/search?fromCityName=Noida&fromCityId=1429&toCityName=" +
+                spinner.getSelectedItem() + "&toCityId=" + targetCityId + "&onward=" + date +
+                "&opId=0&busType=Any";
+
+        return url;
     }
 }
