9128d5a06c2171b8bf801f4c7fe76b20b1965f58
Ayush Dubey <codeayush1998@gmail.com>
2019-03-02 22:02:29 +0530

    Bug Fix

--- app/src/main/java/in/gov/sih/mycity/LoginActivity.java
+++ app/src/main/java/in/gov/sih/mycity/LoginActivity.java
@@ -38,6 +38,15 @@ public class LoginActivity extends AppCompatActivity {
     private FirebaseAuth.AuthStateListener mAuthStateListener;
 
     @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        Intent intent=new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_HOME);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
