a2a65ae586831dec773ac0f28023730e3c5c1fa8
Prajwal <2017ugcs059@nitjsr.ac.in>
2019-03-03 16:46:12 +0530

    Bug Fixes #9

--- app/src/main/java/in/gov/sih/mycity/MainActivity.java
+++ app/src/main/java/in/gov/sih/mycity/MainActivity.java
@@ -39,6 +39,11 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -93,6 +98,22 @@ public class MainActivity extends EasyLocationAppCompatActivity {
         //startActivity(new Intent(this, DetailsActivity.class));finish();
         user = findViewById(R.id.user);
         user.setText(FirebaseAuth.getInstance().getCurrentUser().getDisplayName());
+
+        final DatabaseReference dref=FirebaseDatabase.getInstance().getReference("karma");
+         dref.addListenerForSingleValueEvent(new ValueEventListener() {
+             @Override
+             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                 if(!dataSnapshot.hasChild(FirebaseAuth.getInstance().getUid()))
+                 {
+                     dref.getRef().child(FirebaseAuth.getInstance().getUid()).setValue(0);
+                 }
+             }
+
+             @Override
+             public void onCancelled(@NonNull DatabaseError databaseError) {
+
+             }
+         });
 
         InputStream inputStream = getResources().openRawResource(R.raw.cities);
         ParseCity city = new ParseCity(inputStream);
--- app/src/main/java/in/gov/sih/mycity/ProfileActivity.java
+++ app/src/main/java/in/gov/sih/mycity/ProfileActivity.java
@@ -2,19 +2,29 @@ package in.gov.sih.mycity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class ProfileActivity extends AppCompatActivity {
 
-    private TextView nameView;
+    private TextView nameView,karmaView,logOut;
     private TextView emailView;
     private ImageView imageView;
     private Context context;
@@ -34,6 +44,21 @@ public class ProfileActivity extends AppCompatActivity {
         Glide.with(context).load(FirebaseAuth.getInstance().getCurrentUser().getPhotoUrl()).into(imageView);
         home = (LinearLayout) findViewById(R.id.home);
         profile = (LinearLayout) findViewById(R.id.profile);
+        karmaView = (TextView) findViewById(R.id.karma);
+        logOut = (TextView) findViewById(R.id.logout);
+        final DatabaseReference dref=FirebaseDatabase.getInstance().getReference("karma");
+        dref.getRef().child(FirebaseAuth.getInstance().getUid()).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                int karma=dataSnapshot.getValue(Integer.class);
+                karmaView.setText(""+karma);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
         home.setAlpha(0.3f);
         profile.setAlpha(1f);
 
@@ -44,6 +69,21 @@ public class ProfileActivity extends AppCompatActivity {
             }
         });
 
+        logOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AuthUI.getInstance()
+                        .signOut(ProfileActivity.this)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            public void onComplete(@NonNull Task<Void> task) {
+                                // user is now signed out
+                                startActivity(new Intent(ProfileActivity.this, LoginActivity.class));
+                                finish();
+                            }
+                        });
+                Toast.makeText(ProfileActivity.this, "Signed out!", Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
 }
